14ff54169d0f81cd55df421353e581279002f624
phananh271105 <phananh271105@gmail.com>
2019-09-28 19:26:10 +0700

    mathtestv2

--- Python/Session04/Part5/mathgame.py
+++ Python/Session04/Part5/mathgame.py
@@ -1,16 +1,68 @@
 import random
 
-
+fs = 0
 for i in range (10):
-    x = random.randint(30, 60)
-    y = random.randint(40, 80)
+    x = random.randint(3, 10)
+    y = random.randint(4, 12)
     a = random.randint(-1, 1)
-    z = x + y + a
-    print (x , "+", y, "=", z)
-    t = input("True or False?")
-    if t == "T" and z == x + y:
-        print("Right")
-    elif t == "F" and z != x + y:
-        print ("Right")
-    else:
-        print("Wrong")
+    op = random.randint(0,3)
+    if op == 0 :
+        z = x + y + a
+        print (x , "+",y , "=",z)
+        t = input ("True or False?")
+        if t == "T" and z == x + y:
+            print("Right")
+            fs = fs + 1
+            print ("Score: ",fs)
+        elif t == "F" and z != x + y:
+            print ("Right")
+            fs = fs + 1
+            print ("Score: ",fs)
+        else:
+            print("Wrong")
+            print ("Score: ",fs)
+    elif op == 1:
+        z = x - y + a
+        print (x , "-" ,y, "=",z)
+        t = input ("True or False?")
+        if t == "T" and z == x - y:
+            print("Right")
+            fs = fs + 1
+            print ("Score: ",fs)
+        elif t == "F" and z != x - y:
+            print ("Right")
+            fs = fs + 1
+            print ("Score: ",fs)
+        else:
+            print("Wrong")
+            print ("Score: ",fs)
+    elif op == 2:
+        z = x*y + a
+        print (x , "*", y, "=",z)
+        t = input ("True or False?")
+        if t == "T" and z == x * y:
+            print("Right")
+            fs = fs + 1
+            print ("Score: ",fs)
+        elif t == "F" and z != x * y:
+            print ("Right")
+            fs = fs + 1
+            print ("Score: ",fs)
+        else:
+            print("Wrong")
+            print ("Score: ",fs)
+    elif op == 3:
+        z = x/y + a
+        print (x , "/" , y, "=",z)
+        t = input ("True or False?")
+        if t == "T" and z == x / y:
+            print("Right")
+            fs = fs + 1
+            print ("Score: ",fs)
+        elif t == "F" and z != x / y:
+            print ("Right")
+            fs = fs + 1
+            print ("Score: ",fs)
+        else:
+            print("Wrong")
+            print ("Score: ",fs)
